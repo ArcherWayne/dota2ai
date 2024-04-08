2ad1bdcd83046c82598d26d169c37bebbb506823
Archer Wayne <archerwayne1216@gmail.com>
2024-04-08 23:12:20 +0800

    add grimstroke item list.

---
+++ hero/items/grimstroke.lua
@@ -0,0 +1,25 @@
+----------------------------------------------------------------------------
+--	Ranked Matchmaking AI v1.7.17
+--	Author: Archer Wayne		Email:archerwayne1216@gmail.com
+----------------------------------------------------------------------------
+--导入通用函数库
+local X = {}
+
+X.ItemsToBuy =
+{
+	"item_tango",
+	"item_branches",
+	"item_branches",
+	"item_mantle",
+	"item_circlet",
+	"item_recipe_null_talisman", --无用挂件
+	"item_magic_stick", --大魔棒
+	"item_boots",
+	"item_arcane_boots",
+	"item_aether_lens",
+	"item_glimmer_cape", --微光
+	"item_force_staff",
+	"item_guardian_greaves"
+}
+
+return X
